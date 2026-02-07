5d2f36420c785d382ab8ec999ad98ee2b554b657
Samarendra <samarendrapani1998@gmail.com>
2026-02-07 10:30:24 +0100

    Introducing new class HaplotypeMapper - Serves as a map between the NxN haplotype pairs possible to a reduced linear vector of states selected based on prior genotype likelihoods. - Removes a lot of unused space from previous implementations where states were just kept as 0 but memory was allocated.

---
+++ src/haplotypemapper.cpp
@@ -0,0 +1,116 @@
+#include <numeric>
+
+#include "haplotypemapper.h"
+
+HaplotypeMapper::HaplotypeMapper(const GenotypeLikelihoods& genotype_likelihoods, const std::vector<int>& allele_references) {
+
+    std::vector<uint32_t> selected_indices = select_genotypes(genotype_likelihoods);
+    std::vector<bool> is_genotype_selected(genotype_likelihoods.size(), false);
+    for (uint32_t idx : selected_indices) {
+        if (idx < is_genotype_selected.size()) {
+            is_genotype_selected[idx] = true;
+        }
+    }
+    bool all_genotypes_selected = selected_indices.size() == genotype_likelihoods.size();
+    uint32_t n_haplotypes = allele_references.size();
+    haplotype_to_selected_map.remake(n_haplotypes, n_haplotypes, -1);
+    
+    std::vector<uint32_t> sorted_alleles;
+    sorted_alleles.reserve(2);
+    uint32_t count = 0;
+    
+    for (uint32_t i = 0; i < n_haplotypes; i++) {
+        for (uint32_t j = 0; j < n_haplotypes; j++) {
+            int allele_i = allele_references.at(i);
+            int allele_j = allele_references.at(j);
+            if (allele_i == -1 || allele_j == -1) { continue; } // if one of the alleles is invalid or not available, don't consider that pair.
+            sorted_alleles.clear();
+            if (allele_i < allele_j) {
+                sorted_alleles.push_back((uint32_t)allele_i);
+                sorted_alleles.push_back((uint32_t)allele_j);
+            }
+            else {
+                sorted_alleles.push_back((uint32_t)allele_j);
+                sorted_alleles.push_back((uint32_t)allele_i);
+            }
+            uint32_t index = convert_alleles_to_index(sorted_alleles);
+            /**
+             * index is added if
+             *  - all genotypes have been selected.
+             *  - if this was one of the selected genotypes.
+             */
+            bool allowed = all_genotypes_selected || (index < is_genotype_selected.size() && is_genotype_selected[index]);
+            if (allowed) {
+                reverse_map.push_back({i, j});
+                haplotype_to_selected_map.set(i, j, count);
+                count++;
+            }
+        }
+    }
+}
+
+std::vector<uint32_t> HaplotypeMapper::select_genotypes(const GenotypeLikelihoods& genotype_likelihoods) const {
+
+    std::vector<std::pair<long double, uint32_t>> indexed_values;
+    uint32_t gl_size = genotype_likelihoods.size();
+    indexed_values.reserve(gl_size);
+
+    long double sum = 0.0L;
+    for (uint32_t i = 0; i < gl_size; i++) {
+        long double value = genotype_likelihoods.get_by_index(i);
+        indexed_values.push_back({value, i});
+        sum += value;
+    }
+
+    if (sum == 0.0L) {
+        /**
+         * no genotype likelihood values available yet.
+         * returning all genotypes as possible
+         */
+        std::vector<uint32_t> result(gl_size);
+        std::iota(std::begin(result), std::end(result), 0);
+        return result;
+    }
+
+    /**
+     * sorting the genotype likelihoods in descending order.
+     */
+    std::sort(indexed_values.begin(), indexed_values.end(), 
+        [](const std::pair<long double, uint32_t>& a, const std::pair<long double, uint32_t>& b) {
+            return a.first > b.first; 
+        }
+    );
+
+    std::vector<uint32_t> result;
+    long double current_sum = 0.0L;
+    const long double THRESHOLD = 0.9L;
+
+    uint32_t count = 0;
+    for (const auto& item : indexed_values) {
+        current_sum += item.first;
+        result.push_back(item.second);
+        count += 1;
+        if (current_sum > THRESHOLD) {
+            break;
+        }
+    }
+
+    return result;
+
+     
+}
+
+uint32_t HaplotypeMapper::get_num_states() const {
+    return reverse_map.size();
+}
+
+int HaplotypeMapper::get_state_index(uint32_t i, uint32_t j) const {
+    assert(i < haplotype_to_selected_map.get_size0());
+    assert(j < haplotype_to_selected_map.get_size1());
+    return haplotype_to_selected_map.at(i, j);
+}
+
+std::pair<uint32_t, uint32_t> HaplotypeMapper::get_haplotypes_indices(uint32_t i) const {
+    assert(i < reverse_map.size());
+    return reverse_map.at(i);
+}
---
+++ src/haplotypemapper.h
@@ -0,0 +1,66 @@
+#ifndef HAPLOTYPEMAPPER_H
+#define HAPLOTYPEMAPPER_H
+
+#include "genotypingalgorithm.h"
+#include "vector2d.h"
+#include "genotype.h"
+
+/**
+ * @brief Tracks the subset of haplotype pairs to consider for genotypes.
+ * 
+ * After one round of genotyping, the subsequent rounds of genotyping can use the previously
+ * calculated genotype likelihoods to avoid unlikely genotypes.
+ * HaplotypeMapper uses the genotype likelihoods and selects genotypes which are likely.
+ * Then it maps the all the haplotype pairs that have the selected genotypes. 
+ * 
+ * This will be used to reduce the NxN space that each biparition (defined by BiparitionIterator)
+ * in the HMM needs, where N is the number of haplotypes.
+ * 
+ * Let the number of selected haplotype pairs be M where NxN > M. In this class, we will refer to this
+ * as "linearized reduced-space states"
+ * 
+ * @see BipartitionIterator
+ * @note This class is hardcoded for ploidy 2
+ */
+class HaplotypeMapper {
+
+    public:
+
+        HaplotypeMapper(const GenotypeLikelihoods& genotype_likelihoods, const std::vector<int>& allele_references);
+
+        // returns the size of the linearized reduced-space states, i.e. M
+        uint32_t get_num_states() const;
+
+        // get the index in linearized reduced-space states from the haplotype pair
+        int get_state_index(uint32_t i, uint32_t j) const;
+
+        // get the haplotype pair from the index in the linearized reduced-space
+        std::pair<u_int32_t, u_int32_t> get_haplotypes_indices(uint32_t i) const;
+
+
+
+    private:
+
+        /**
+         * Selects the genotypes which are "likely" and returns a vector of their canonical index.
+         * The likely genotypes are the m genotypes with the highest likelihoods scores such that sum of their scores >= 0.9
+         */
+        std::vector<uint32_t> select_genotypes(const GenotypeLikelihoods& genotype_likelihoods) const;
+
+        /**
+         * This maps the NxN haplotype pairs to index of the linearized reduced-space states.
+         * The vector stores -1 for the haplotype pairs which were not selected.
+         */
+        Vector2D<int> haplotype_to_selected_map;
+        
+        /**
+         * The "Reverse Map" stores the map of the linearized reduced-space states to 
+         * the original NxN matrix of haplotype pairs
+         */
+         std::vector<std::pair<u_int32_t, u_int32_t>> reverse_map;
+
+};
+
+
+
+# endif // HAPLOTYPEMAPPER_H
