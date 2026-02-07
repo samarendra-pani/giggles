694e4b9161ece53d75e79e91f8b16380715144e9
Samarendra <samarendrapani1998@gmail.com>
2026-02-07 10:21:19 +0100

    writing separate class for emission prob calculation for better documentation and testing

---
+++ src/emissionprobabilitycomputer.cpp
@@ -0,0 +1,102 @@
+#include <cassert>
+
+#include "emissionprobabilitycomputer.h"
+
+EmissionProbabilityComputer::EmissionProbabilityComputer(uint32_t n_alleles) {
+    emission_probability_table = Vector2D<long double>(n_alleles, n_alleles, 1.0L);
+}
+
+long double EmissionProbabilityComputer::at(uint32_t i, uint32_t j) const {
+    return emission_probability_table.at(i, j);
+}
+
+void EmissionProbabilityComputer::update_emission_probability(const int cluster_bit_changed, const BipartitionIterator& iterator, std::vector<const Entry *>& entries) {
+    uint32_t n_alleles = emission_probability_table.get_size0();
+	if (cluster_bit_changed >= 0) {
+		/**
+		 * A cluster has been flipped since the last call to this function.
+		 */
+		std::unordered_map<uint32_t, bool> changed_reads = iterator.get_changed_reads(cluster_bit_changed);
+		for (auto const& [entry_index, newBit]: changed_reads) {
+			// Need to check if the entry being changed actually corresponds to the correct read id.
+			assert(entries.at(entry_index)->get_read_id() == iterator.get_parent_column()->get_read_ids()->at(entry_index));
+			if (entries.at(entry_index)->get_allele_type() == Entry::BLANK) {
+				/**
+				 * The flipped entry has no effect on emission. It is blank.
+				 */
+				continue;
+			}
+			bool oldBit = !newBit;
+			for (uint32_t i = 0; i < n_alleles; i++) {
+				for (uint32_t j = 0; j < n_alleles; j++) {
+					long double numerator;
+					long double denominator;
+					if (newBit) {
+						/**
+						 * The new bit is 1. So the entry was at bipartion 0 (corresponding to allele i) and is now at bipartition 1 (corresponding to allele j).
+						 */
+						numerator = entries.at(entry_index)->get_emission_scores()[j];
+						denominator = entries.at(entry_index)->get_emission_scores()[i];
+					} 
+					else {
+						/**
+						 * The new bit is 0. So the entry was at bipartion 1 (corresponding to allele j) and is now at bipartition 0 (corresponding to allele i).
+						 */
+						numerator = entries.at(entry_index)->get_emission_scores()[i];
+						denominator = entries.at(entry_index)->get_emission_scores()[j];
+					}
+					long double new_value = emission_probability_table.at(i, j) * (numerator/denominator);
+					emission_probability_table.set(i, j, new_value);
+				}
+			}
+		}	
+	}
+	else {
+		/**
+		 * Initialization case.
+		 * The  is at the first bipartition.
+		 */
+		uint32_t index = iterator.get_read_cluster_bit_representation(); // this is value showing the bipartition of every read cluster.
+		/**
+		 * Iterating through all allele pairs
+		 */
+		for (uint32_t i = 0; i < n_alleles; i++) {
+			for (uint32_t j = 0; j < n_alleles; j++) {
+				uint32_t cluster_count = 0;
+				long double value = 1.0L;
+				/**
+				 * Finding the cluster ids in the current column.
+				 * The bits of index correspond to these cluster ids.
+				 */
+				std::vector<uint32_t>* cluster_ids = iterator.get_parent_column()->get_read_cluster_ids();
+				for (uint32_t c_id: *cluster_ids) {
+					/**
+					 * For each cluster, get the read indices corresponding to that cluster.
+					 * These read indices correspond to the indices of their respective entries.
+					 */
+					std::vector<uint32_t> read_indices = iterator.get_read_index_from_cluster_id(c_id);
+					// Determine whether the current cluster is in bipartition 0 or 1
+					bool bit = (index & (1 << cluster_count)) != 0;
+					for (uint32_t r_idx: read_indices) {
+						if (entries.at(r_idx)->get_allele_type() == Entry::BLANK) {
+							continue;
+						}
+						if (bit) {
+							// If read at r_idx is in biparition 1 then value gets multiplied with emission from allele j
+							value = value * (entries.at(r_idx)->get_emission_scores()[j]);
+						}
+						else {
+							// If read at r_idx is in biparition 0 then value gets multiplied with emission from allele i
+							value = value * (entries.at(r_idx)->get_emission_scores()[i]);
+						}
+					}
+					// Move to the next cluster
+					cluster_count++;
+				}
+				// Setting the computed emission probability value
+				emission_probability_table.set(i, j, value);
+			}
+		}
+	}
+}
+    
---
+++ src/emissionprobabilitycomputer.h
@@ -0,0 +1,57 @@
+#ifndef EMISSIONPROBABILITYCOMPUTER_H
+#define EMISSIONPROBABILITYCOMPUTER_H
+
+#include "vector2d.h"
+#include "bipartitioniterator.h"
+#include "entry.h"
+
+/**
+ * @brief Computes the emission probability matrix
+ *
+ * Given some bipartition of reads at a variant position, this matrix stores at position i, j
+ * the probability of the reads in biparition 1 having allele i and reads in biparition 2 having allele j.
+ */
+class EmissionProbabilityComputer {
+
+    public:
+
+        EmissionProbabilityComputer(uint32_t n_alleles);
+        
+        /**
+         * Get the emission probability for allele pair (i, j)
+         */
+        long double at(uint32_t i, uint32_t j) const;
+
+        /**
+         * Given a bit that has changed in the bipartition defined by the BiparitionIterator,
+         * we update the emission probabilities.
+         * What value does emission_probability_table(i, j) have?
+         * 
+         * INITIALIZATION: if (bit_changed < 0) --> this indicates that no bit was changed and that the bipartion is the first bipartition we are looking at.
+         *      - We look at the bit assignment of all the clusters which are in .
+         *      - The bipartition is defined by the b_index.
+         *      - Based on the bipartion of the clusters, we find the reads inside the clusters and assign then emission score i if they are bipartition 0 otherwise emission score j.
+         * UPDATE: if (bit_changed >= 0) --> this indicates that some bit was changed.
+         *      - Since Initialization already created a starting emission probability table, this code block just updates the table.
+         *      - The bit that was flippped corresponds to a cluster. We extract the reads that were present in the cluster.
+         *      - If read was flipped from bipartition 0 to 1, we divide the table value with emission score i and multiply with emission score j
+         */
+        void update_emission_probability(const int bit_changed, const BipartitionIterator& iterator, std::vector<const Entry *>& entries);
+
+
+    private:
+
+        /**
+         * A 2D table of size n x n where n is the number of alleles at that position.
+         * The value at (i, j) gives the following value:
+         * Given a bipartition of reads B = {B0, B1} where B0 corresponds to reads assigned to haplotype 0 and B1 corresponds to reads assigned to haplotype 1.
+         * We have the assignment of reads in B0 to allele i and reads in B1 to allele j.
+         * The value at (i, j) gives the probability of observing the reads in their assigned alleles given this bipartition and allele assignment.
+         */
+        Vector2D<long double> emission_probability_table;
+
+
+};
+
+
+#endif // EMISSIONPROBABILITYCOMPUTER_H
