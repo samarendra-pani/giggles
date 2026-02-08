e48f102f363f8bc5d41d7cfdcdabd52916681a69
Samarendra <samarendrapani1998@gmail.com>
2026-02-08 18:22:17 +0100

    pre-calcuting the products to save time later

--- src/transitionprobabilitycomputer.h
+++ src/transitionprobabilitycomputer.h
@@ -1,21 +1,30 @@
 #ifndef TRANSITIONPROBABILITYCOMPUTER_H
 #define TRANSITIONPROBABILITYCOMPUTER_H
 
+struct TransitionProbabilities {
+    long double p2;
+    long double q2;
+    long double pq;
+};
+
 /**
- * Calculates the pr and qr values of the Li-Stephens transition probility model.
- * pr -> result.first
- * qr -> result.second
- * 
+ * Calculates the pr and qr values of the Li-Stephens transition probility model: pr and qr.
+ * Then it stores it in the struct TransitionProbabilities.
+ *  
  * Note:
  * We do not normalize these values since we do an overall normalization at the end.
  */
-std::pair<long double, long double>  calculate_transition_probabilities(const float& recombcost, const uint32_t& num_haplotypes) {
+TransitionProbabilities calculate_transition_probabilities(const float& recombcost, const uint32_t& num_haplotypes) {
     long double s = (long double)num_haplotypes;
     long double r = (long double)recombcost;
     
-    std::pair<long double, long double> result;
-    result.first = (1.0 - exp(-(r/s))) / s;         // calculating pr
-    result.second = exp(-(r/s)) + result.first;     // calculating qr
+    long double p = (1.0 - exp(-(r/s))) / s;   // calculating pr
+    long double q = exp(-(r/s)) + p;           // calculating qr
+
+    TransitionProbabilities result;
+    result.p2 = pow(p, 2);
+    result.q2 = pow(q, 2);
+    result.pq = p*q;
 }
 
 #endif // TRANSITIONPROBABILITYCOMPUTER_H
